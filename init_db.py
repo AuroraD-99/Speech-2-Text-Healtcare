02150772e85efd51b482908eb2ed300640d9ef78
Gennaro_Iannicelli <gen.iannicelli@studenti.unina.it>
2025-06-10 10:45:40 +0200

    Readme.mc

--- init_db.py
+++ init_db.py
@@ -5,10 +5,17 @@ from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+    db = DB()
     load_dotenv("key.env", override=True)
     dataset_path = os.getenv("dataset_path")
+    
+    flag = db.collection('init_flag').find_one({"initialized": True})
+    
+    if flag:
+        exit(0)
+    
     if dataset_path:
-        db = DB()
         db.popola_db(dataset_path)
+        db.collection('init_flag').insert_one({"initialized": True})
     else:
         print("❌ Variabile 'dataset_path' non trovata nel file .env")
